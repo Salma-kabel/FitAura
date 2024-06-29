1b536c2f0b598bd1f236112d5e7e6294f965b160
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-29 21:19:07 +0300

    add exercises routes to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sequelize = require('./config/database');
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protected');
+const exercisesRoutes = require('./routes/exercises');
 
 require('dotenv').config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
+app.use('/api/exercises', exercisesRoutes);
 
 sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
