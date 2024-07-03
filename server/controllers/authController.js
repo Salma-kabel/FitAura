baf3fd5560c20a5552289ad509d93d2dc5c1cf4c
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-07-04 00:19:29 +0300

    Auth Updated (Validation secured)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    console.log(req.body)
     const hashedPassword = await bcrypt.hash(password, 10);
 
     if (await User.findOne({ where: { email } })) {
@@ -26,11 +25,14 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ error: 'Invalid email or password' });
     }
+
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
     res.json({ token });
   } catch (error) {
     res.status(400).json({ error: error.message });
--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
+    req.user = { userId: decoded.userId}
     next();
   } catch (error) {
     return res.status(401).json({ error: 'Token invalid' });
