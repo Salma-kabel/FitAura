8b4c7737bdd012ca5eb3341780fb4f1fbd5d81d7
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-07-05 01:30:09 +0300

    Some improvements about logging and retrieving exercises

--- server/controllers/exerciseController.js
+++ server/controllers/exerciseController.js
@@ -1,53 +1,12 @@
 const Exercise = require('../models/Exercise');
-const UserExercise = require('../models/UserExercise');
+const RoutineExercise = require('../models/RoutineExercise');
 const User = require('../models/User');
 
-exports.addExerciseToRoutine = async (req, res) => {
-  const { exerciseId } = req.body;
-  const userId = req.user.id;
-
-  try {
-    const exercise = await Exercise.findByPk(exerciseId);
-    if (!exercise) {
-      return res.status(404).json({ error: 'Exercise not found' });
-    }
-
-    const userExercise = await UserExercise.create({
-      userId,
-      exerciseId,
-    });
-
-    res.status(201).json(userExercise);  
-  } catch (error) {
-    console.error('Error adding exercise to routine:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.getUserRoutine = async (req, res) => {
-  const userId = req.user.id;
-
-  try {
-    const userExercises = await UserExercise.findAll({
-      where: { userId },
-      include: [{ model: Exercise }],
-    });
-    res.status(200).json(userExercises);
-  } catch (error) {
-    console.error('Error retrieving user routine:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
 exports.logExercise = async (req, res) => {
-  const { userId, name, duration, description, caloriesBurned, date } = req.body;
+  const { name, duration, description, caloriesBurned, date } = req.body;
+  const userId = req.user.id;
 
   try {
-    const user = await User.findByPk(userId);
-    if (!user || !user.isAdmin) {
-      return res.status(403).json({ error: 'Unauthorized' });
-    }
-
     const exercise = await Exercise.create({
       userId,
       name,
@@ -67,10 +26,37 @@ exports.getExercises = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    const exercises = await Exercise.findAll({ where: { userId } });
+    const adminExercises = await Exercise.findAll({
+      where: {
+        userId: null,
+      },
+    });
+
+    const userExercises = await Exercise.findAll({
+      where: {
+        userId: userId,
+      },
+    });
+
+    const exercises = [...adminExercises, ...userExercises];
     res.status(200).json(exercises);
   } catch (error) {
     console.error('Error retrieving exercises:', error);
-    res.status(500).json({ error: error.message});
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getUserRoutine = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const userExercises = await RoutineExercise.findAll({
+      where: { userId },
+      include: [{ model: Exercise }],
+    });
+    res.status(200).json(userExercises);
+  } catch (error) {
+    console.error('Error retrieving user routine:', error);
+    res.status(500).json({ error: error.message });
   }
 };
--- server/routes/exercise.js
+++ server/routes/exercise.js
@@ -4,13 +4,11 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const {
   logExercise,
   getExercises,
-  addExerciseToRoutine,
   getUserRoutine,
 } = require('../controllers/exerciseController');
 
 router.post('/log', authMiddleware, logExercise);
 router.get('/', authMiddleware, getExercises);
-router.post('/routine', authMiddleware, addExerciseToRoutine);
 router.get('/routine', authMiddleware, getUserRoutine);
 
 module.exports = router;
